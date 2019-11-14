5fc892dac0e3440eb53d5f4ee8e7b71a62cf4b83
唐龙龙 <tanglonglong@ysstech.com>
2019-11-14 17:06:28 +0800

    初始化

--- client/src/main/java/xyz/supertll/client/ClientApplication.java
+++ client/src/main/java/xyz/supertll/client/ClientApplication.java
@@ -2,15 +2,14 @@ package xyz.supertll.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class EurekaApplication {
+public class ClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(ClientApplication.class, args);
 	}
 
 }
---
+++ client/src/main/java/xyz/supertll/client/test.java
@@ -0,0 +1,25 @@
+package xyz.supertll.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tanglonglong ＼(－－)／
+ * @version 1.0
+ * @date 2019/11/13 20:58
+ */
+
+@RestController
+@RefreshScope
+public class test {
+
+    @Value("${version}")
+    private String version = "开发环境";
+
+    @RequestMapping("/test")
+    public String test() {
+        return "当前环境：" + version;
+    }
+}
